84ce2642058192178c06ce9072d4cb4c712af841
Alex Fay (aka Kim Flynn) <43191440+KimFlynn@users.noreply.github.com>
2020-02-01 00:53:24 -0700

    Movement inheritance class created

--- MovementInherit.cs
+++ MovementInherit.cs
@@ -1,12 +1,17 @@
 //Movement class all code will inherit 
-
+public class Movement{
     public void MoveRightTopCorner(){
         upperCorner = new Vector3 (300f, 300f, 100f); // need to redfine after building wall
         currentPosition = ghost.position(); //look for unity method for this
         if (wall.position == (ghost.position + 2f)){
-            direction = currentPosition - previousPosition;
+            
             //look at unity directions, 
             // from distance if 3right is less than 3 left distance go right
             // reverse for up and down
+
+
+               
+
         }
     }
+}
