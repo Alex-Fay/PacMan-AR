fb88ce6e2fc0734b1a9eed7d7daae1b3005fbf05
Alex Fay (aka Kim Flynn) <43191440+KimFlynn@users.noreply.github.com>
2020-02-01 02:05:30 -0700

    Added MoveRight Algorithm for Scatter Mode

--- MovementInherit.cs
+++ MovementInherit.cs
@@ -1,17 +1,30 @@
 //Movement class all code will inherit 
+//TODO: look into class inheritance for Mode Change Mode()
 public class Movement{
-    public void MoveRightTopCorner(){
-        upperCorner = new Vector3 (300f, 300f, 100f); // need to redfine after building wall
-        currentPosition = ghost.position(); //look for unity method for this
-        if (wall.position == (ghost.position + 2f)){
-            
-            //look at unity directions, 
-            // from distance if 3right is less than 3 left distance go right
-            // reverse for up and down
+    Stopwatch stopwatch = new Stopwatch();
+    scatterTime = 7;
 
+    public void MoveRightTopCorner(scatterTime){
+        if(stop.Elapsed < scatterTime){
+            upperCorner = new Vector3 (300f, 300f, 100f); // need to redfine after building wall
+            currentPosition = ghost.position(); //look for unity method for this
+            if (wall.position == (ghost.position + 4f)){
+                rotate += 1;
 
-               
-
+                if (rotate % 2 == 0){
+                transform.Rotate(Vector3.right, 90, 1);
+                }
+                else{
+                    transform.Rotate(Vector3.left, 90, 1);
+                    transform.Translate(Vector3.forward * speed);
+                }
+            }
+            else{
+                transform.Translate(Vector3.forward * speed);
+            }
+        }
+        else{
+            Mode();
         }
     }
 }
