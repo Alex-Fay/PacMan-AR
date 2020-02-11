de6601a813b47b0d3f9ac3fad98ee6bdb42b9f3d
Alex Fay (aka Kim Flynn) <43191440+KimFlynn@users.noreply.github.com>
2020-02-10 21:30:27 -0700

    added red Chase Method

--- Ghost/PacMan-RedGhostMovement.cs
+++ Ghost/PacMan-RedGhostMovement.cs
@@ -7,8 +7,9 @@ using System.Timers;
 //TODO: add camera changes to PacMan Camera.main.transform.position = ...
 //develop pacman start position, set up grid tiles 28*36
 
-public class RedMovement : MonoBehaviour
+public class RedMovement : MonoBehaviour, MovementInterface
 {
+    CharecterController CharecterController
     public GameObject pacman;
     public GameObject redGhost;
     public GameObject wall;
@@ -16,6 +17,8 @@ public class RedMovement : MonoBehaviour
     private pacmanStartPos = new Vector3(0.0f, 0.0f, 30.0f); //update when finalized
     private currentLevel; //get from GameStart class
     private float redGhostSpeed = 3.9f;
+    public bool gameOver = false;
+
 
     void Awake(){
         //outside ghost pen starting location
@@ -33,46 +36,51 @@ public class RedMovement : MonoBehaviour
         stopwatch.Start();
 
         //ghosts alternate between scatter and chase for 4 cycles like original
-        scatterTime = 7;
+        int scatterTime = 7;
+        int scatterCallNum = 0;
 
         for(i=0; i<4; i++){
             scatterTime = scatterTime - i;
-            Scatter();
+            Scatter(scatterTime, scatterCallNum);
         }
 
-       public void Chase(chaseTime){
-           //define chaseTime and pacman positions
-       }
-
-        public void MoveRightTopCorner(){
-            upperCorner = new Vector3 (300f, 300f, 100f); // need to redfine after building wall
-            currentPosition = ghost.position(); //look for unity method for this
-            if (wall.position == (ghost.position + 2f)){
-                direction = currentPosition - previousPosition;
-                //look at unity directions, 
-                // from distance if 3right is less than 3 left distance go right
-                // reverse for up and down
-            }
-        }
-
-        public int Scatter(scatterTime){
+        void Scatter(scatterTime, scatterCallNum, gameOver){
+            scatterCallNum += 1;
+            if(gameOver = true){break;}
             //origanal scatter method has red to top right corner
             //pink = left top  orange = bottom left    green = bottom right
             
-            if (stopwatch.Elapsed < scatterTime){
-                previousPosition = ghost.position();
-                MoveRightTopCorner();
+            while (stopwatch.Elapsed < scatterTime){
+                int rotateX_Movement = 90;
+                int rotateY_corner = 1;
+                MovementInterface.scatterCorner(scatterTime, rotateX_Movement, rotateY_corner);
             }
             else{
-                Chase();
+                    MovementInterface.Chase(-1);
             }
         }
+
+        void Chase(chaseTime, gameOver){
+            // Chase method very different for each ghost
+            if (chaseTime != 0){
+                chaseTime -= 1;
+                Vector2 ghostChasePoint = Vector2.MoveTowards(transform.position() , pacman.position(), speed);
+                GetComponent<RedGhost>().MovePosition(ghostChasePoint);
+                if(gameOver = true){
+                    break;
+                }
+            }
+            else{
+                Chase(chaseTime, gameOver)
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 
 //next do chase method
